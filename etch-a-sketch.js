24b0b10b57b8208050e3a33dce34d52edef5928a
Austin Hogan <austinhogandev@gmail.com>
2023-08-24 12:49:19 -0400

    Dynamically create 16x16 grid using DOM & for loop

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,38 +1,22 @@
 const display = document.querySelector('.eas-display');
-const row1 = document.createElement('div');
-row1.classList.add('row');
-const col1 = document.createElement('div');
-col1.classList.add('col');
-const content1 = document.createElement('div');
-content1.textContent = "1";
-col1.appendChild(content1);
-row1.appendChild(col1);
 
-const col2 = document.createElement('div');
-col2.classList.add('col');
-const content2 = document.createElement('div');
-content2.textContent = "2";
-col2.appendChild(content2);
-row1.appendChild(col2);
+const GRID_HEIGHT = 16;
+const GRID_WIDTH = 16;
 
-display.appendChild(row1);
+for (let x = 1; x <= GRID_HEIGHT; x++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
 
-const row2 = document.createElement('div');
-row2.classList.add('row');
-const col3 = document.createElement('div');
-col3.classList.add('col');
-const content3 = document.createElement('div');
-content3.textContent = "3";
-col3.appendChild(content3);
-row2.appendChild(col3);
+    for (let y = 1; y <= GRID_WIDTH; y++) {
+        const col = document.createElement('div');
+        col.classList.add('col');
 
-const col4 = document.createElement('div');
-col4.classList.add('col');
-const content4 = document.createElement('div');
-content4.textContent = "4";
-col4.appendChild(content4);
-row2.appendChild(col4);
-
-display.appendChild(row2);
+        const content = document.createElement('div');
+        content.textContent = `${y}`;
+        col.appendChild(content);
+        row.appendChild(col);
+    }
+    display.append(row);
+}
 
 
